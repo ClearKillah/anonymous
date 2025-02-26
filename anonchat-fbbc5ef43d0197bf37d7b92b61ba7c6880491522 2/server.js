d7c41d90ffcee31a06829824b94a037bf45426be
Kent <64082447+ClearKillah@users.noreply.github.com>
2025-02-26 20:54:10 +0300

    Update server.js

--- anonchat-fbbc5ef43d0197bf37d7b92b61ba7c6880491522 2/server.js
+++ anonchat-fbbc5ef43d0197bf37d7b92b61ba7c6880491522 2/server.js
@@ -1,3 +1,6 @@
+// Для корректной работы с прокси Railway
+app.set('trust proxy', true);
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
